73ba3ec44b22f6194f1485a60d8a136bf98e0d27
Carlos Augusto <carlos.aumaca@gmail.com>
2023-02-25 00:39:49 -0300

    feat: add About view

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container-md p-3">
+    <!-- Title -->
+    <h1 class="display-3 mb-3">About</h1>
+
+    <div class="px-4 py-3 text-center">
+      <h1 class="d-block mx-auto mb-4"><i class="ri-star-half-line ms-2"></i></h1>
+      <h1 class="display-5 fw-bold">Discover new good anime</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">
+          Are you in struggle to find a decent anime nowadays?<br>
+          Here in AnimeRating, you are able to
+          find the animes reviews and check for good animes that will not waste your time!
+        </p>
+        <div class="d-flex justify-content-center">
+          <a href="/reviews" class="btn btn-outline-success btn-lg px-4 d-flex align-items-center gap-2">See reviews <i
+              class="ri-arrow-right-line"></i></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
